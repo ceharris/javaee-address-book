763a2f61110970df012abbfddb80a50a6531cf2b
Carl Harris <ceharris@vt.edu>
2014-06-09 11:10:23 -0400

    implemented ContactRepository findAll

--- src/main/java/ceh/demo/repository/JpaContactRepository.java
+++ src/main/java/ceh/demo/repository/JpaContactRepository.java
@@ -1,8 +1,11 @@
 package ceh.demo.repository;
 
+import java.util.Collection;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import ceh.demo.Contact;
 
@@ -15,6 +18,13 @@ public class JpaContactRepository implements ContactRepository {
   @Override
   public void add(Contact contact) {
     entityManager.persist(contact);
+  }
+
+  @Override
+  public Collection<Contact> findAll() {
+    TypedQuery<Contact> query = entityManager.createNamedQuery(
+        "findAllContacts", Contact.class);
+    return query.getResultList();
   }
 
 }
